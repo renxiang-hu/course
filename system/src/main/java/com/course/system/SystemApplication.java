4e6d89e096412f11e1b6e32de5e987c951d53a35
胡仁祥 <18917734365@163.com>
2022-08-04 22:37:03 +0800

    1、将System注册到eureka

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -2,8 +2,10 @@ package com.course.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
 
 	public static void main(String[] args) {
