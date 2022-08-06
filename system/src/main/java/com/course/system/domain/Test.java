71ba17e65f93ec5139f499531ce025b83d7d8a8d
胡仁祥 <18917734365@163.com>
2022-08-06 16:04:54 +0800

    1、增加三层架构

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,13 @@
+package com.course.system.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test {
+    private String id;
+    private String name;
+}
---
+++ system/src/main/java/com/course/system/service/TestService.java
@@ -0,0 +1,20 @@
+package com.course.system.service;
+
+import com.course.system.domain.Test;
+import com.course.system.mapper.TestMapper;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    private TestMapper testMapper;
+
+    public List<Test> test(){
+        return testMapper.list();
+    }
+}
